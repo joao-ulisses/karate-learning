d99af4ffc9843138c40d47635af070d15c2c731c
João Ulisses <sirnobin@gmail.com>
2023-08-28 16:49:59 -0300

    cucumber reporting

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -5,21 +5,39 @@ import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+
 import org.junit.jupiter.api.Test;
 
 class ConduitTest {
 
-    // @Test
-    // void testParallel() {
-    //     Results results = Runner.path("classpath:conduitApp")
-    //             //.outputCucumberJson(true)
-    //             .parallel(5);
-    //     assertEquals(0, results.getFailCount(), results.getErrorMessages());
-    // }
-
-    @Karate.Test
-    Karate testTags() {
-        return Karate.run().tags("@debug").relativeTo(getClass());
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:conduitApp")
+                .outputCucumberJson(true)
+                .parallel(2);
+        generateReport(results.getReportDir());
+        assertTrue(results.getFailCount() == 0, results.getErrorMessages());
     }
 
+    // @Karate.Test
+    // Karate testTags() {
+    //     return Karate.run().tags("@debug").relativeTo(getClass());
+    // }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "conduitApp");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
